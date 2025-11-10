3c11ebee1cf6a813472ca03ebc202dbd09c1c16a
Yuri_S <zebrawild@gmail.com>
2025-11-10 10:13:33 +0200

    added registerSchema for validating api requests, added universal validateRequest.ts,

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,11 +1,22 @@
 import express from 'express'
 import { checkEmailExists, registerGoogleUser, registerUser } from '../controllers/authController'
 import verifyToken from '../middleware/verify-token'
+import validateRequest from '../middleware/validateRequest'
 
 const router = express.Router()
 
-router.post('/', registerUser)
+router.post(
+  '/',
+  validateRequest('auth/register.schema.json', 'body'),
+  registerUser
+)
+
 router.post('/check-email', checkEmailExists)
-router.post('/with-google', verifyToken, registerGoogleUser)
+
+router.post(
+  '/with-google',
+  verifyToken,
+  registerGoogleUser
+)
 
 export default router
