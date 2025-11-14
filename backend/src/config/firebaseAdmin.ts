4b1dce58e31d8231f55b2eeb32ccacf34b013f95
Yuri_S <zebrawild@gmail.com>
2025-11-14 11:58:53 +0200

    update firebaseCongig.ts

--- backend/src/config/firebaseAdmin.ts
+++ backend/src/config/firebaseAdmin.ts
@@ -6,17 +6,20 @@ dotenv.config();
 if (!admin.apps.length) {
   if(process.env.MODE === 'development') {
     // admin.initializeApp();
-    const serviceAccount = JSON.parse(
-      Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64!, 'base64').toString('utf8')
-    );
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    })
+    // const serviceAccount = JSON.parse(
+    //   Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64!, 'base64').toString('utf8')
+    // );
+    // admin.initializeApp({
+    //   credential: admin.credential.cert(serviceAccount),
+    // })
   } else if(
-    process.env.MODE === 'production' &&
-    process.env.GOOGLE_CREDENTIALS_JSON
+    process.env.MODE === 'production'
   ) {
-    const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON)
+    // const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON)
+    const serviceAccount = JSON.parse(
+      Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS!, 'base64').toString('utf8')
+    );
+
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
     })
