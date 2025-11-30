ee5f8d368e9632da1c2d07994798d093584182c7
Yuri_S <zebrawild@gmail.com>
2025-12-01 01:15:14 +0200

    started search in market form

--- frontend/src/app/shared/components/header-market/components/search-in-market/search-in-market.component.ts
+++ frontend/src/app/shared/components/header-market/components/search-in-market/search-in-market.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { TranslateModule } from '@ngx-translate/core';
+import { BtnIconComponent } from '../../../../ui-elems/buttons/btn-icon/btn-icon.component';
 
 @Component({
   selector: 'app-search-in-market',
-  imports: [],
+  imports: [
+    TranslateModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    BtnIconComponent
+  ],
   templateUrl: './search-in-market.component.html',
   styleUrl: './search-in-market.component.scss'
 })
 export class SearchInMarketComponent {
 
+  searchField = new FormControl('')
+
+  onSubmit() {
+
+  }
+
+  clearInput(){
+    this.searchField.setValue('')
+  }
 }
