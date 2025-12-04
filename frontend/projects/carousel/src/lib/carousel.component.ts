306d526200f62998eb0f0623c9ea94df0787a381
Yuri_S <zebrawild@gmail.com>
2025-12-04 23:33:24 +0200

    added swipe to slider for mobile and desctop versions

--- frontend/projects/carousel/src/lib/carousel.component.ts
+++ frontend/projects/carousel/src/lib/carousel.component.ts
@@ -1,15 +1,14 @@
-import { Component, computed, Input, OnDestroy, OnInit, signal } from '@angular/core';
+import { Component, computed, ElementRef, HostListener, inject, Input, OnDestroy, OnInit, Renderer2, signal, ViewChild } from '@angular/core';
 import { CarouselSlide } from './carousel.types';
-import { RouterLink } from '@angular/router';
 import { NgStyle } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { interval, Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'lib-carousel',
   imports: [
-    RouterLink,
     NgStyle,
     MatIconModule,
     MatButtonModule
@@ -19,16 +18,50 @@ import { interval, Subscription } from 'rxjs';
 })
 export class CarouselComponent implements OnInit, OnDestroy {
   @Input() slides!: CarouselSlide[];
-  @Input() autoplay = true;
+  @Input() autoplay = false;
   @Input() carouselHeight = '500px';
   @Input() autoPlayInterval = 5000 // автопрокрутка каждые 5 сек
   @Input() restoreAutoPlayTime = 10000 // автопрокрутка каждые 5 сек
+  @Input() swipeEnabled = true // Включить/выключить свайпы
+  @Input() swipeMinDistance = 50 // Минимальное расстояние для свайпа (в пикселях)
+  @Input() draggingEnabled = true
+  @Input() dragMinDistance = 50
+
+  private readonly renderer = inject(Renderer2)
+  private readonly router = inject(Router)
+
+  @ViewChild('carouselList') carouselList!: ElementRef<HTMLElement>
 
   currentIndex = signal(0)
   currentSlide = computed(() => this.slides[this.currentIndex()])
 
+  // Для обработки touch событий
+  private touchStartX = 0
+  private touchEndX = 0
+  private touchStartY = 0
+  private touchEndY = 0
+  private isSwiping = false
+
+  // Для обработки mouse событий
+  private mouseStartX = 0
+  private mouseEndX = 0
+  private mouseStartY = 0
+  private mouseEndY = 0
+  isDragging = false
+
   private timerId: any
   private autoPlaySub?: Subscription
+
+  @HostListener('window:keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key == 'ArrowLeft') {
+      this.prev()
+      this.onUserInteraction()
+    } else if (event.key == 'ArrowRight') {
+      this.next()
+      this.onUserInteraction
+    }
+  }
 
   ngOnInit() {
     if (this.autoplay && this.slides.length > 1) {
@@ -86,11 +119,110 @@ export class CarouselComponent implements OnInit, OnDestroy {
     this.currentIndex.set(i)
   }
 
+  // ============ TOUCH EVENTS (Mobile) ============
+  onTouchStart(event: TouchEvent) {
+    if (!this.swipeEnabled) return
+
+    this.touchStartX = event.changedTouches[0].screenX
+    this.touchStartY = event.changedTouches[0].screenY
+
+    this.isSwiping = true
+  }
+
+  onTouchMove(event: TouchEvent) {
+    if (!this.swipeEnabled || !this.isSwiping) return
+  }
+
+  onTouchEnd(event: TouchEvent) {
+    if (!this.swipeEnabled || !this.isSwiping) return
+
+    this.touchEndX = event.changedTouches[0].screenX
+    this.touchEndY = event.changedTouches[0].screenY
+    this.handleSwipe()
+    this.isSwiping = false
+  }
+
+  handleSwipe() {
+    const deltaX = this.touchStartX - this.touchEndX
+    const deltaY = this.touchStartY - this.touchEndY
+
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      if (deltaX < -this.swipeMinDistance) {
+        this.prev()
+      } else if (deltaX > this.swipeMinDistance) {
+        this.next()
+      }
+      this.onUserInteraction()
+    }
+  }
+  // ============ END TOUCH EVENTS (Mobile) ============
+
+
+  // ============ MOUSE EVENTS (Desktop) ============
+  onMouseDown(event: MouseEvent) {
+    if (!this.draggingEnabled) return
+    
+    this.mouseStartX = event.screenX
+    this.mouseStartY = event.screenY
+    this.isDragging = true
+
+    event.preventDefault()
+  }
+
+  onMouseMove(event: MouseEvent) {
+    if (!this.draggingEnabled || !this.isDragging) return
+  }
+
+  onMouseUp(event: MouseEvent) {
+    if (!this.draggingEnabled || !this.isDragging) return
+    this.mouseEndX = event.screenX
+    this.mouseEndY = event.screenY
+    this.handleDragging()
+    this.isDragging = false
+  }
+
+  onMouseLeave(event: MouseEvent) {
+    if (!this.isDragging) return
+
+    this.isDragging = false
+  }
+
+  private handleDragging() {
+    const deltaX = this.mouseStartX - this.mouseEndX
+    const deltaY = this.mouseStartY - this.mouseEndY
+    if ((Math.abs(deltaX) > Math.abs(deltaY))) {
+      if (deltaX < -this.dragMinDistance) {
+        this.next()
+        this.onUserInteraction()
+      } else if (deltaX > this.dragMinDistance) {
+        this.prev()
+        this.onUserInteraction()
+      }
+    }
+  }
+
+  navigateByLink(link: string) {
+    const deltaX = this.mouseStartX - this.mouseEndX
+    const deltaY = this.mouseStartY - this.mouseEndY
+
+    if (
+      (Math.abs(deltaX) < this.dragMinDistance)
+      && (Math.abs(deltaY) < this.dragMinDistance)
+    ) {
+      this.router.navigate([link], {
+        queryParamsHandling: 'merge'
+      }).then()
+    }
+
+  }
+
   ngOnDestroy() {
     this.stopAutoplay()
-    if(this.timerId) {
+    if (this.timerId) {
       clearTimeout(this.timerId)
       this.timerId = null
     }
+    this.isDragging = false
+    this.isSwiping = false
   }
 }
--- frontend/projects/carousel/src/lib/carousel.service.ts
+++ frontend/projects/carousel/src/lib/carousel.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
+import { CarouselSlide } from './carousel.types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarouselService {
+  // slides = signal<CarouselSlide[]>([])
+  // currentIndex = signal(0)
+  // currentSlide = computed(() => this.slides[this.currentIndex()])
 
-  constructor() { }
+  
 }
