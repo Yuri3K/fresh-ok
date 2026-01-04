b03a39f6a9a536bd7f825e882d4e806c8d637ad3
Yuri_S <zebrawild@gmail.com>
2026-01-04 14:12:36 +0200

    added preloader for waiting when langs will be get from server

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,4 +3,10 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    const preloader = document.getElementById('app-preloader');
+    if (preloader) {
+      preloader.remove();
+    }
+  })
   .catch((err) => console.error(err));
