01f64af14bc3a1e6fcbff72ee5329fb453cfd154
Yuri_S <zebrawild@gmail.com>
2026-01-07 17:26:40 +0200

    finished sponsors carousel

--- frontend/src/app/routes/home/components/sponsors-carousel/sponsors-carousel.component.ts
+++ frontend/src/app/routes/home/components/sponsors-carousel/sponsors-carousel.component.ts
@@ -2,12 +2,14 @@ import { Component, inject } from '@angular/core';
 import { SponsorsService } from './services/sponsors.service';
 import { SponsorsSlideComponent } from './sponsors-slide/sponsors-slide.component';
 import { NgxCarouselComponent } from 'ngx-freshok-carousel';
+import { MiniFabBtnComponent } from '../../../../shared/ui-elems/buttons/mini-fab-btn/mini-fab-btn.component';
 
 @Component({
   selector: 'app-sponsors-carousel',
   imports: [
     SponsorsSlideComponent,
     NgxCarouselComponent,
+    MiniFabBtnComponent
   ],
   templateUrl: './sponsors-carousel.component.html',
   styleUrl: './sponsors-carousel.component.scss'
