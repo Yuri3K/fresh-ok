e3b4027c303a968bd9eaa91b53fa4ecc0f586d65
Yuri_S <zebrawild@gmail.com>
2026-01-14 22:33:02 +0200

    refactored mobile styles for product cards

--- frontend/src/app/routes/products/products.routes.ts
+++ frontend/src/app/routes/products/products.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
   {
     path: '',
     component: ProductsComponent,
+  },
+  {
+    path: ':id',
+    loadComponent: () => import('./components/product-detail/product-detail.component').then(m => m.ProductDetailComponent)
   }
 ]
