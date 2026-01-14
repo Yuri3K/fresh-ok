c94fe41a91c5fd58230458daec0ac37eccf970df
Yuri_S <zebrawild@gmail.com>
2026-01-14 14:48:09 +0200

    added tooltip for prroduct card

--- frontend/src/app/shared/components/product-card/product-card.component.ts
+++ frontend/src/app/shared/components/product-card/product-card.component.ts
@@ -1,21 +1,12 @@
 import { Component, computed, inject, input } from '@angular/core';
 import { Product } from '../../../core/services/products.service';
-import {
-  Lang,
-  LangCode,
-  LangsService,
-} from '../../../core/services/langs.service';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { filter, map } from 'rxjs';
 import { MiniFabBtnComponent } from '../../ui-elems/buttons/mini-fab-btn/mini-fab-btn.component';
 import { MEDIA_URL } from '../../../core/urls';
 import { RouterLink } from '@angular/router';
-import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
 import { CounterComponent } from '../counter/counter.component';
 import { GetCurrentLangService } from '../../../core/services/get-current-lang.service';
 import { CalcDiscountPipe } from '../../../core/pipes/calc-discount.pipe';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-product-card',
@@ -24,6 +15,7 @@ import { CalcDiscountPipe } from '../../../core/pipes/calc-discount.pipe';
     RouterLink,
     CounterComponent,
     CalcDiscountPipe,
+    MatTooltipModule,
   ],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
