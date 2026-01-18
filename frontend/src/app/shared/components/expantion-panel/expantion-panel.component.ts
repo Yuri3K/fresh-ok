fe8d03e2e121f930aaf960bbc59a875e24aac614
Yuri_S <zebrawild@gmail.com>
2026-01-18 21:16:12 +0200

    not important

---
+++ frontend/src/app/shared/components/expantion-panel/expantion-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expantion-panel',
+  imports: [],
+  templateUrl: './expantion-panel.component.html',
+  styleUrl: './expantion-panel.component.scss'
+})
+export class ExpantionPanelComponent {
+
+}
