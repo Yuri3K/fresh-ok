e40cf889aa8704bffa900376e5229f9a1d591ec3
Yuri_S <zebrawild@gmail.com>
2026-02-11 23:01:32 +0200

    added mobile styles for product-details popups

--- frontend/src/app/shared/components/dialogs/delivery-and-payment-dialog/delivery-and-payment-dialog.component.ts
+++ frontend/src/app/shared/components/dialogs/delivery-and-payment-dialog/delivery-and-payment-dialog.component.ts
@@ -5,6 +5,7 @@ import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { H5TitleComponent } from '../../../ui-elems/typography/h5-title/h5-title.component';
 import { BtnIconComponent } from '../../../ui-elems/buttons/btn-icon/btn-icon.component';
 import { MEDIA_URL } from '../../../../core/urls';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-delivery-and-payment-dialog',
@@ -14,6 +15,7 @@ import { MEDIA_URL } from '../../../../core/urls';
     H5TitleComponent,
     MatDialogModule,
     BtnIconComponent,
+    MatButtonModule
   ],
   templateUrl: './delivery-and-payment-dialog.component.html',
   styleUrl: './delivery-and-payment-dialog.component.scss',
