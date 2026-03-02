31546959b623dfcbfc113431a9d3e07a9f35afa3
Yuri <zebrawild@gmail.com>
2026-03-02 21:47:37 +0200

    updated some tooltips for light/dark mode

--- frontend/src/app/shared/components/headers/components/switch-mode/switch-mode.component.ts
+++ frontend/src/app/shared/components/headers/components/switch-mode/switch-mode.component.ts
@@ -3,10 +3,11 @@ import { AsyncPipe } from '@angular/common';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BtnIconComponent } from '@shared/ui-elems/buttons/btn-icon/btn-icon.component';
 import { SwitchModeService } from '@core/services/switch-mode.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-switch-mode',
-  imports: [BtnIconComponent, AsyncPipe, MatTooltipModule],
+  imports: [BtnIconComponent, AsyncPipe, MatTooltipModule, TranslateModule],
   templateUrl: './switch-mode.component.html',
   styleUrl: './switch-mode.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
